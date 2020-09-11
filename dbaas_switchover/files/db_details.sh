3b23d7a3f955884ba63290deec5a245d6c520c77
pazmistry <paz_egap@hotmail.co.uk>
2020-09-11 06:56:01 +0100

    now have a nested db_details

--- dbaas_switchover/files/db_details.sh
+++ dbaas_switchover/files/db_details.sh
@@ -3,10 +3,13 @@ ORACLE_SID=$1
 ORAENV_ASK=NO
 export ORACLE_SID ORAENV
 . oraenv -s
-$ORACLE_HOME/bin/sqlplus -S "/ as sysdba" <<EOF
+$ORACLE_HOME/bin/sqlplus -S "/ as sysdba" <<EOF | grep -v "^$"
+/*
 set heading off
 set lin 180
 col statement for a50
-select db_unique_name||':'||open_mode||':'||database_role statement from v\$database;
+select ':'||db_unique_name||':'||open_mode||':'||database_role||':' statement from v\$database;
 exit;
+*/
+start db_details.sql
 EOF
